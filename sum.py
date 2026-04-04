61b533335df3bcc1dd1f6620d786205257285ad5
unknown <dipalimandal0522@gmail.com>
2026-04-04 16:40:53 +0530

    percent of gc in dna

--- sum.py
+++ sum.py
@@ -1,3 +1,6 @@
-a=6
-b=9
-print(a+b)
+#finding the percentage of gc present in dna
+dna="ATGCAGCTAGCTAGCTAGCTAGCTAGCTAGCTAGC"
+gc_count=dna.count("G")+dna.count("C")
+
+gc_percent=(gc_count/len(dna))*100
+print("The percentage of GC in the DNA sequence is:", gc_percent)
