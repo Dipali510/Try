7927b159c7c0580b13e13f8acf332db554dd8abf
unknown <dipalimandal0522@gmail.com>
2026-04-14 10:41:57 +0530

    scoring including empty spaces as -2

--- score.py
+++ score.py
@@ -1,8 +1,12 @@
 from Bio.Align import substitution_matrices
 matrix = substitution_matrices.load("BLOSUM62")
-seq1="AGT"
-seq2="AGT"
+seq1="ATG"
+seq2="AT-"
+gap=-2
 score =0
 for a,b in zip(seq1,seq2):
+   if a=="-" or b=="-":
+    score += gap
+   else:
     score += matrix[a,b]
-print("score is",score)
+print ("score after penalty of -2 for empty space-->",score)
